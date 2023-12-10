2f211bd1dcd3d4d23db562ec3fc2d14b6482609a
pawel.jasinski <pawel.jasinski@amano.ai>
2023-12-10 13:54:56 +0100

    add notification

--- files/notifications.py
+++ files/notifications.py
@@ -1,5 +1,9 @@
+from django.core.mail import send_mail
+
 def send_success_notification(uploaded_file):
-    # change to push/email/sms etc
-    print(
-        f"File {uploaded_file.original_name} successfully processed. S3 URL: {uploaded_file.s3_url}"
-    )
+    subject = 'File Upload Success'
+    message = f'The file {uploaded_file.original_name} has been successfully processed.\nS3 URL: {uploaded_file.s3_url}'
+    from_email = 'email@example.com'
+    recipient_list = ['recipient@example.com']
+
+    send_mail(subject, message, from_email, recipient_list)
