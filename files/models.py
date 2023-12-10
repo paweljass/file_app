92c20d7e84db0cf78fd95f4e28ba4f227953c821
pawel.jasinski <pawel.jasinski@amano.ai>
2023-12-10 13:59:05 +0100

    make changes in model

--- files/models.py
+++ files/models.py
@@ -6,3 +6,10 @@ class UploadedFile(models.Model):
     unique_name = models.CharField(max_length=255, unique=True)
     original_name = models.CharField(max_length=255)
     s3_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.original_name} ({self.s3_url})"
+
+    class Meta:
+        verbose_name = 'Uploaded File'
+        verbose_name_plural = 'Uploaded Files'
